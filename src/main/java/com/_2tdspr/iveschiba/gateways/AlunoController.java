278a70b5d47e50e702a3df2b115c97ee342c7756
Ives <ivesjundi@gmail.com>
2024-08-28 20:00:49 -0300

    feat :sparkles: aula 28/08 validation

--- src/main/java/com/_2tdspr/iveschiba/gateways/AlunoController.java
+++ src/main/java/com/_2tdspr/iveschiba/gateways/AlunoController.java
@@ -5,9 +5,12 @@ import com._2tdspr.iveschiba.gateways.request.AlunoPostRequestDTO;
 import com._2tdspr.iveschiba.gateways.response.AlunoResponseDTO;
 import com._2tdspr.iveschiba.usecases.CadastrarAluno;
 import com._2tdspr.iveschiba.usecases.impl.CadastrarAlunoImpl;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,13 +19,14 @@ import java.util.List;
 @RequestMapping("/aluno/fiap")
 @RequiredArgsConstructor
 // Capaz de fazer um @Autowired sem sujar o codigo com varios @Autowired
+@Validated
 public class AlunoController {
 
 //    @Autowired
     private final CadastrarAluno cadastrarAlunoService;
 
     @PostMapping
-    public ResponseEntity<AlunoResponseDTO> createAluno(@RequestBody AlunoPostRequestDTO aluno){
+    public ResponseEntity<AlunoResponseDTO> createAluno(@RequestBody @Valid AlunoPostRequestDTO aluno){
         Aluno newAluno = cadastrarAlunoService.createAluno(aluno);
 
         AlunoResponseDTO alunoResponseDTO = AlunoResponseDTO.builder().
@@ -37,6 +41,11 @@ public class AlunoController {
 
     @GetMapping
     public ResponseEntity<List<Aluno>> getAlunos(){
+        return ResponseEntity.status(200).build();
+    }
+
+    @GetMapping("{alunoId}")
+    public ResponseEntity<Aluno> getAluno(@PathVariable("alunoId") String id){
         return ResponseEntity.status(200).build();
     }
 
--- src/main/java/com/_2tdspr/iveschiba/gateways/request/AlunoPostRequestDTO.java
+++ src/main/java/com/_2tdspr/iveschiba/gateways/request/AlunoPostRequestDTO.java
@@ -1,8 +1,16 @@
 package com._2tdspr.iveschiba.gateways.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.Length;
+
 public record AlunoPostRequestDTO(
         String id,
         String rm,
+        @NotBlank
+        @Length(min = 4)
         String primeiroNome,
         String sobrenome,
         String documento,
